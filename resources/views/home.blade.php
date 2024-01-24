f260c4661404431e2e0587acee0349b07de018d7
Avinashpoll <avinashpal1532@gmail.com>
2024-01-24 17:44:54 +0530

    testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @include('layouts.sidebar')
 
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
@@ -12,15 +12,13 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
+              <li class="breadcrumb-item active">Dashboard</li>
             </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+          </div>
+        </div>
+      </div>
     </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
+    <!-- #testing purpose -->
     <section class="content">
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
@@ -29,14 +27,12 @@
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-              <!-- <h3>{{ $destinationCount }}</h3> -->
+              <h3>55</h3> 
 
-                <p>Destination</p>
+                <p>User</p>
               </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              <a href="{{ route('destination_list') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -44,14 +40,12 @@
             <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-                 <!-- <h3>{{ $packageCount }}</h3> -->
+                 <h3>10</h3>
 
                 <p>Holiday Packages</p>
               </div>
-              <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-              </div>
-              <a href="{{ route('package_list') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -63,9 +57,7 @@
 
                 <p>Blogs</p>
               </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
+              
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
@@ -74,14 +66,12 @@
             <!-- small box -->
             <div class="small-box bg-danger">
               <div class="inner">
-                 <!-- <h3>{{ $contactCount }}</h3> -->
+                 <h3>15</h3>
 
                 <p>Contact Us</p>
               </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              <a href="{{ route('contact_list') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+             
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
          
